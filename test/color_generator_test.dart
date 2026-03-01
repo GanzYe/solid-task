7c79d4d6b344cda70edf0d626f2feb81d77fe951
ganzye <bahadirov4@gmail.com>
2026-03-01 22:33:28 +0100

    test: add test for ColorGenerator

---
+++ test/color_generator_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:solid_task/utils/color_generator.dart';
+
+void main() {
+  group('ColorGenerator Unit Tests', () {
+    test('generateRandomColor returns a valid opaque Color', () {
+      final color = ColorGenerator.generateRandomColor();
+
+      expect(color, isA<Color>());
+
+      expect(color.a, equals(1.0));
+
+      expect(color.toARGB32(), greaterThanOrEqualTo(0xFF000000));
+      expect(color.toARGB32(), lessThanOrEqualTo(0xFFFFFFFF));
+    });
+
+    test(
+      'generateRandomColor generates different colors over multiple calls',
+      () {
+        final color1 = ColorGenerator.generateRandomColor();
+        final color2 = ColorGenerator.generateRandomColor();
+
+        expect(color1, isNot(equals(color2)));
+      },
+    );
+  });
+}
